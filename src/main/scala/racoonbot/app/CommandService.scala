1bdb54d33d64176149e98bc2676d02926e7859dd
Viacheslav Kisel <primazp@icloud.com>
2015-09-12 19:48:23 +0300

    Fast fix

--- src/main/scala/racoonbot/app/CommandService.scala
+++ src/main/scala/racoonbot/app/CommandService.scala
@@ -10,9 +10,9 @@ class CommandService(val body: JValue) {
   def run() {
     logger.info("Checking message for command presence...")
     try {
-      val command = compact(body \ "message" \ "text").split(" ")(0)
+      val command = compact(body \ "message" \ "text").split(" ")(0).substring(1) // cut arguments and leading "
       val chatId = compact(body \ "message" \ "chat" \ "id")
-
+      logger.info("command: " + command)
       command match {
         case "/start" => start(chatId)
         case "/help" => help(chatId)
