cab28e87797e3d01383d6a6879e677963fd17f49
Viacheslav Kisel <primazp@icloud.com>
2015-09-12 16:43:59 +0300

    Parse body and send request to id

--- src/main/scala/racoonbot/app/ApiRequest.scala
+++ src/main/scala/racoonbot/app/ApiRequest.scala
@@ -6,12 +6,12 @@ import scalaj.http.Http
 class ApiRequest {
   val API_URL = "https://api.telegram.org/bot133163653:AAEIPp8IA7xgxh2dqUlWTX3W90RbSS5kJRU/"
 
-  def perform(method: String, args: Map): Unit = {
-    Http(API_URL + method)
-      .param("q", "monkeys").asString
-  }
+//  def perform(method: String): Unit = {
+//    Http(API_URL + method)
+//      .param("q", "monkeys").asString
+//  }
 
-  def sendMessage(chatId: Int, text: String): Unit = {
+  def sendMessage(chatId: String, text: String): Unit = {
     Http(API_URL + "sendMessage").param("chat_id", chatId.toString).param("text", text).asString
   }
 }
