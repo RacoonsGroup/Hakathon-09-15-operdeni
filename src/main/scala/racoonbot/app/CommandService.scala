a92ada8e61576637d62f81a1b381222bff2e9fbe
Viacheslav Kisel <primazp@icloud.com>
2015-09-12 18:42:49 +0300

    Add moo method to CommandService

--- src/main/scala/racoonbot/app/CommandService.scala
+++ src/main/scala/racoonbot/app/CommandService.scala
@@ -2,6 +2,7 @@ package racoonbot.app
 
 import org.json4s.JsonAST.JValue
 import org.slf4j.LoggerFactory
+import org.json4s.jackson.JsonMethods._
 
 class CommandService(val body: JValue) {
   val logger =  LoggerFactory.getLogger(getClass)
@@ -15,6 +16,7 @@ class CommandService(val body: JValue) {
   }
 
   def moo(): Unit = {
-
+    val chatId = compact(body \ "message" \ "chat" \ "id")
+    ApiRequest.sendMessage(chatId, "There are no Easter Eggs in this program.")
   }
 }
